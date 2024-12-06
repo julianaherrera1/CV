1e1172388f3b23c6035c80868990a1e8785c78b3
Juliana Herrera <julianaherrera522@gmail.com>
2024-12-06 12:28:27 -0500

    feat: (services-section): Designed the services section with responsive support - Created the services section with a modern design using a purple color palette. - Added three accordion items to showcase services: Frontend Development, Basic Backend, and Agile Project Collaboration. - Integrated custom icons for each service using Font Awesome and Heroicons. - Ensured full responsiveness for an optimal experience on both mobile and larger screens.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ module.exports = {
   content: [
     "./index.html", // Apunta al archivo index.html
     "./src/**/*.{html,js}", // Si tienes archivos JS o HTML dentro de src (aunque no mencionas que los tengas, es una buena práctica)
+    './node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
